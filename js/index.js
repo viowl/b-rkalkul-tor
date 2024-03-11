aaa464cd0c9b94bb823684f2a22149968cb22e2b
viowl <sovaworkbox@gmail.com>
2024-03-12 00:58:30 +0100

    make form

--- js/index.js
+++ js/index.js
@@ -1,51 +1,15 @@
-const result = document.querySelector('.result');
+const result = document.querySelector(".result");
 console.log(result.textContent);
-const salary = document.getElementById('name-salary');
-const day = document.getElementById('name-day');
-const night = document.getElementById('name-night');
-const button = document.querySelector('.button');
-const overday = document.getElementById('name-over-day');
-const overnight = document.getElementById('name-over-night');
-// salary.addEventListener('input', () => {
-//     // Получить текущее значение инпута
-//     const inputValue = salary.value;
-  
-//     // Теперь переменная inputValue содержит текущее значение инпута
-//     console.log('Текущее значение ЗП: ' + inputValue);
-//   });
+const salaryPerHour = document.getElementById("salary-per-hour");
+const day = document.getElementById("day-hour");
+const daySalary = document.getElementById("day-salary");
 
-// //   отслеживание инпут с днями 
-// day.addEventListener('input', () => {
+salaryPerHour.addEventListener("input", calculateDaySalary);
+day.addEventListener("input", calculateDaySalary);
 
-//     const inputDay = day.value;
-//     console.log('количество дневных смен:' + inputDay)
-//   });
+function calculateDaySalary() {
+  const salary = salaryPerHour.value;
+  const hours = day.value;
 
-// night.addEventListener('input', () => {
-// const inputNight = night.value;
-// console.log('кол-во ночей:' + inputNight)
-// })
-// console.log(salary.value)
-function makeCalculatev () {
-    const inputValue = salary.value;
-    const inputDay = day.value;
-    const inputNight = night.value;
-    const od = overday.value * 2;
-    const on = overnight.value * 2;
-    const allDay = inputValue * 12 * inputDay;
-    const allNight = (inputValue * 12 * inputNight) * 1.35;
-    const allOverDay = (od * 12) * inputValue;
-    const allOverNight = ((on * 12) * inputValue) * 1.35;
-
-
-
-    const inputResult = (allDay + allNight + allOverDay + allOverNight) * 0.665;
-
-   result.textContent = inputResult 
-    
-console.log(inputResult)
-
+  return (daySalary.textContent = (salary * hours).toFixed(2));
 }
-
-// кнопка
-button.addEventListener('click', makeCalculatev)
