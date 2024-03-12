ec85fad66f72872e9e2b0d61b27990fd3b6be306
viowl <sovaworkbox@gmail.com>
2024-03-12 01:44:54 +0100

    Update index.js
    added logic for calculating the first line

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-const result = document.querySelector(".result");
-console.log(result.textContent);
 const salaryPerHour = document.getElementById("salary-per-hour");
 const day = document.getElementById("day-hour");
 const daySalary = document.getElementById("day-salary");
 
-salaryPerHour.addEventListener("input", calculateDaySalary);
 day.addEventListener("input", calculateDaySalary);
 
-function calculateDaySalary() {
+function calculateDaySalary(event) {
+  const dayHour = event.currentTarget.value;
+  console.log(dayHour);
   const salary = salaryPerHour.value;
-  const hours = day.value;
-
-  return (daySalary.textContent = (salary * hours).toFixed(2));
+  console.log(salary);
+  if (dayHour !== "") {
+    daySalary.textContent = `${salary * dayHour} HUF`;
+  }
 }
